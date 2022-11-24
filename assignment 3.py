d24a9e98747a3deace79887afd63ff6b55be5855
rajesh6334 <118992210+rajesh6334@users.noreply.github.com>
2022-11-24 20:24:49 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,12 @@
+#student pass or fail
+sno=int(input('enter serial no'))
+name=input('enter name')
+group=input('enter group')
+print('enter the marks')
+s1=int(input('enter the  marks'))
+s2=int(input('enter the marks'))
+s3=int(input('enter the marks'))
+if s1>=35&s2>=35&s3>=35:
+    print('student pass')
+else:
+    print('student fail')
